3307ddaf7c2c129b238efa27193abc5cb4dfaa08
Adam Kobi <adamkobi12@gmail.com>
2019-10-02 11:35:02 +0300

    done tests works (#1)
    * done tests works
    
    * fixed linting issues
    
    * change names in tests

---
+++ fixtures/index.js
@@ -0,0 +1,22 @@
+module.exports = [
+    [
+        '{"line":"2019-09-24T11:20:44.503+0000 I COMMAND  [conn47945] command db_name.db_collection command: find { find: "db_collection", filter: { user_id: 123413412, full_name: { $nin: [ "", null ] } }, sort: { _id: 1 }, limit: 1, singleBatch: true, $db: "db_name", $clusterTime: { clusterTime: Timestamp(1569324043, 85), signature: { hash: BinData(0, WIERD_HASH123123123AASDA, keyId: 123455677) } }, lsid: { id: UUID("1111111-222222-333333-444444-555555") } } planSummary: IXSCAN { user_id: 1 } keysExamined:125530 docsExamined:125530 hasSortStage:1 cursorExhausted:1 numYields:990 nreturned:0 reslen:225 locks:{ Global: { acquireCount: { r: 1982 } }, Database: { acquireCount: { r: 991 } }, Collection: { acquireCount: { r: 991 } } } protocol:op_msg 545ms","namespace":"db_name.db_collection"},',
+        {
+            timestamp: Date.parse('2019-09-24T11:20:44.503+0000'),
+            severity: 'I',
+            operation: 'COMMAND',
+            connection_id: 'conn47945',
+            db_name: 'db_name',
+            collection_name: 'db_collection',
+            general_command: 'find',
+            mongo_command: '{ find: "db_collection", filter: { user_id: 123413412, full_name: { $nin: [ "", null ] } }, sort: { _id: 1 }, limit: 1, singleBatch: true, $db: "db_name", $clusterTime: { clusterTime: Timestamp(1569324043, 85), signature: { hash: BinData(0, WIERD_HASH123123123AASDA, keyId: 123455677) } }, lsid: { id: UUID("1111111-222222-333333-444444-555555") } } planSummary: IXSCAN { user_id: 1 } keysExamined:125530 docsExamined:125530 hasSortStage:1 cursorExhausted:1 numYields:990 nreturned:0 reslen:225 locks:{ Global: { acquireCount: { r: 1982 } }, Database: { acquireCount: { r: 991 } }, Collection: { acquireCount: { r: 991 } } }',
+            took_millis: 545,
+            num_returned: 0,
+            num_yields: 990,
+            result_length: 225,
+            keys_examined: 125530,
+            docs_examined: 125530,
+            protocol: 'op_msg'
+        }
+    ]
+];
---
+++ index.js
@@ -0,0 +1,48 @@
+const numerify = require('./lib/numerify');
+const {pattern} = require('./lib/pattern');
+
+const NUMERIC_FIELDS = [
+    'took_millis',
+    'num_returned',
+    'num_yields',
+    'result_length',
+    'keys_examined',
+    'docs_examined'
+];
+
+module.exports = (string) => {
+    const match = pattern.exec(string);
+
+    if (!match) {
+        return {};
+    }
+
+    const [, timestamp, severity, operation, connection_id, db_name, collection_name, general_command, mongo_command, keys_examined, docs_examined, num_yields, num_returned, result_length, protocol, took_millis] = match;
+
+    return process({
+        timestamp,
+        severity,
+        operation,
+        connection_id,
+        db_name,
+        collection_name,
+        general_command,
+        mongo_command,
+        keys_examined,
+        docs_examined,
+        num_yields,
+        num_returned,
+        result_length,
+        protocol,
+        took_millis
+    });
+};
+
+function process(entry) {
+    // convert elasticsearch timestamp to unix timestamp
+    entry.timestamp = Date.parse(entry.timestamp.replace(',', '.'));
+
+    numerify(entry, NUMERIC_FIELDS);
+
+    return entry;
+}
