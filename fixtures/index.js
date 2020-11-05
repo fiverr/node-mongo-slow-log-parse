a771e00ced081c210e8d2ce9fd733f6f6f77c746
Adam <adamkobi12@gmail.com>
2020-11-05 09:20:18 +0200

    bump version and add more fixtures

--- fixtures/index.js
+++ fixtures/index.js
@@ -14,5 +14,21 @@ module.exports = [
             resp_length: 225,
             protocol: 'op_msg'
         }
+    ],
+    [
+        '{"line": "2020-11-03T02:54:26.895+0000 I COMMAND  [conn755878] command db_name.db_collection command: findAndModify { findAndModify: "db_collection", query: { _id: "112223333iadoiasd01230" }, update: { $set: { updated_at: new Date(1604372065945) } }, new: true, maxTimeMS: 10000, $db: "db_name", $clusterTime: { clusterTime: Timestamp(1604372065, 6109), signature: { hash: BinData(0, 123109310924102410294), keyId: 591823498123489123 } }, txnNumber: 1238123, lsid: { id: UUID("1111111-f5a3-222222-953b-333333333") } } planSummary: IDHACK keysExamined:1 docsExamined:1 nMatched:1 nModified:1 keysInserted:1 keysDeleted:1 numYields:0 reslen:571 locks:{ Global: { acquireCount: { r: 4, w: 4 } }, Database: { acquireCount: { w: 4 } }, Collection: { acquireCount: { w: 2 } }, oplog: { acquireCount: { w: 2 } } } protocol:op_msg 944ms"},',
+        {
+            timestamp: 1604372066895,
+            severity: 'I',
+            operation: 'COMMAND',
+            connection_id: 'conn755878',
+            namespace: 'db_name.db_collection',
+            command: 'findAndModify',
+            action: '{ findAndModify: "db_collection", query: { _id: "112223333iadoiasd01230" }, update: { $set: { updated_at: new Date(1604372065945) } }, new: true, maxTimeMS: 10000, $db: "db_name", $clusterTime: { clusterTime: Timestamp(1604372065, 6109), signature: { hash: BinData(0, 123109310924102410294), keyId: 591823498123489123 } }, txnNumber: 1238123, lsid: { id: UUID("1111111-f5a3-222222-953b-333333333") } } planSummary: IDHACK keysExamined:1 docsExamined:1 nMatched:1 nModified:1 keysInserted:1 keysDeleted:1 numYields:0 reslen:571 locks:{ Global: { acquireCount: { r: 4, w: 4 } }, Database: { acquireCount: { w: 4 } }, Collection: { acquireCount: { w: 2 } }, oplog: { acquireCount: { w: 2 } } }',
+            took_millis: 944,
+            num_yields: 0,
+            resp_length: 571,
+            protocol: 'op_msg'
+        }
     ]
 ];
